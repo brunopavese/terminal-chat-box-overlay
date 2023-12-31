6ef2ea245466208cd52d52aa967df6bd3bbaafd6
Bruno Pavese <bruno@ejcm.com.br>
2023-12-31 15:00:12 -0300

    chat box created

---
+++ README.md
@@ -0,0 +1,24 @@
+# Twitch Chat Box Overlay with ComfyJS
+
+## Overview
+This repository contains a customizable Twitch Chat Box Overlay created using the ComfyJS module by the talented user [instafluff](https://github.com/instafluff). ComfyJS simplifies Twitch chat interaction, providing a seamless chat experience for streamers.
+
+## Features
+- **ComfyJS Integration:** Built on the robust ComfyJS module, ensuring reliable Twitch chat integration.
+- **Terminal-Inspired Design:** The chat box adopts the aesthetic of a standard Linux terminal, providing a unique and familiar look for users.
+- **Dracula Theme:** Embrace the visually appealing Dracula color scheme, enhancing the overall appearance of the chat box with its dark and vibrant tones.
+- **Customization:** Easily customize the overlay to match your stream's branding and style.
+
+## Usage
+1. **Install ComfyJS:** Follow the instructions in the [ComfyJS repository](https://github.com/instafluff/ComfyJS) to integrate the module into your project.
+2. **Clone this Repository:** Clone this repository to your local machine.
+3. **Configure Settings:** Adjust settings, such as channel name and appearance preferences, in the configuration file.
+4. **Run the Overlay:** Launch the overlay on your streaming software, and enjoy an interactive and visually appealing chat experience.
+
+## Contributions
+Contributions and feedback are welcome! If you have ideas for improvements or new features, feel free to submit pull requests or open issues.
+
+## Credits
+- [ComfyJS](https://github.com/instafluff/ComfyJS) by instafluff
+
+Elevate your Twitch streaming experience with this terminal-themed chat box overlay, combining the power of ComfyJS with the aesthetics of the popular Dracula theme. Happy streaming!
---
+++ index.js
@@ -0,0 +1,83 @@
+var chat = document.querySelector("#chat>ul");
+
+const flagMapping = {
+  broadcaster: {
+    flagIcon: " ",
+    flagIconColor: "#ff5555",
+    flagLabel: "root"
+  },
+  mod: {
+    flagIcon: "󱢾 ",
+    flagIconColor: "#ffb86c",
+    flagLabel: "mod"
+  },
+  highlighted: {
+    flagIcon: " ",
+    flagIconColor: "#f1fa8c",
+    flagLabel: "user"
+  },
+  subscriber: {
+    flagIcon: " ",
+    flagIconColor: "#ff79c6",
+    flagLabel: "sub"
+  },
+  vip: {
+    flagIcon: "󰇈 ",
+    flagIconColor: "#bd93f9",
+    flagLabel: "vip"
+  },
+  default: {
+    flagIcon: "~",
+    flagIconColor: "#8be9fd",
+    flagLabel: "user"
+  }
+};
+
+function determineFlag(flags) {
+  const flagPriority = ["broadcaster", "mod", "subscriber", "vip", "highlighted"];
+  for (const flag of flagPriority) {
+    if (flags[flag]) {
+      return flag;
+    }
+  }
+
+  return "default";
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const writer = async (textElement, message) => {
+  for (let i = 0; i < message.length; i++) {
+    textElement.textContent += message[i];
+    await sleep(12);
+  }
+}
+
+ComfyJS.onChat = async (user, message, flags, self, extra) => {
+  var newMessage = document.createElement("li");
+  var userName = document.createElement("span");
+  var caracteres = document.createElement("span");
+  var textMessage = document.createElement("span");
+  console.log(extra)
+
+  var flag = determineFlag(flags);
+
+  userName.innerText = `${user}@${flagMapping[flag].flagLabel}`
+  userName.classList.add("user-name");
+
+  caracteres.innerHTML = `:<span class="flag-icon">${flagMapping[flag].flagIcon}</span>$ `
+
+  caracteres.querySelector('.flag-icon').style.color = flagMapping[flag].flagIconColor;
+
+  newMessage.appendChild(userName);
+  newMessage.appendChild(caracteres);
+  newMessage.appendChild(textMessage);
+
+  chat.appendChild(newMessage);
+
+  await writer(textMessage, message);
+}
+
+ComfyJS.Init('YOUR-TWITCH-USER');
